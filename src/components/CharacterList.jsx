e7bc9927fa04fa270fcdef248ddf04791766eca2
aidanar <aidanarfer@outlook.es>
2024-05-23 13:35:43 +0200

    add sort

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,6 +3,8 @@ import CharacterCard from "./CharacterCard"
 export const CharacterList = (props) => {
   // Map que renderiza el componente CharacterCard con cada elemento del array de personajes
 
+  props.filteredData.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+
   return (
     <section>
       {props.filteredData.map((chara) => <CharacterCard key={chara.id} chara={chara} />)}
